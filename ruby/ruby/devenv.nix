52ab366bb8a9a650e9ce0da6f3454f5cf4d29a59
Rafael Mendonça França <rafael.franca@shopify.com>
2026-09-09 10:01:09 -0400

    Add ruby config

---
+++ ruby/ruby/devenv.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  packages = with pkgs; [
+    autoconf
+    automake
+    cargo
+    git
+    gmp
+    gnumake
+    gperf
+    libffi
+    libtool
+    libyaml
+    openssl
+    pkg-config
+    readline
+    ruby
+    rustc
+    zlib
+  ];
+
+  tasks."ruby:build" = {
+    exec = ''
+      set -euo pipefail
+      unset BUNDLE_GEMFILE BUNDLE_PATH CONFIGURE_ARGS GEM_HOME GEM_PATH RUBYLIB RUBYOPT
+      ./autogen.sh
+      mkdir -p build
+      (
+        cd build
+        ../configure -C --disable-install-doc
+        make -j"''${NIX_BUILD_CORES:-1}"
+      )
+    '';
+  };
+
+  tasks."ruby:test" = {
+    after = [ "ruby:build" ];
+    exec = ''
+      set -euo pipefail
+      unset BUNDLE_GEMFILE BUNDLE_PATH CONFIGURE_ARGS GEM_HOME GEM_PATH RUBYLIB RUBYOPT
+      RUBY_TEST_TIMEOUT_SCALE=10 PRECHECK_BUNDLED_GEMS=no \
+        make -C build -j"''${NIX_BUILD_CORES:-1}" check
+    '';
+  };
+}
